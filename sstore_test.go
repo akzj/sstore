f4af7b4885e36829c33dbf6a3fe551c67fc863f8
akzi <382018309@qq.com>
2020-07-23 21:30:21 +0800

    update test

--- sstore_test.go
+++ sstore_test.go
@@ -9,6 +9,9 @@ import (
 
 func TestOpen(t *testing.T) {
 	_ = os.RemoveAll("data")
+	defer func() {
+		_ = os.RemoveAll("data")
+	}()
 	sstore, err := Open(DefaultOptions("data"))
 	if err != nil {
 		t.Fatalf("%+v", err)
@@ -30,41 +33,47 @@ func TestOpen(t *testing.T) {
 }
 
 func TestRecover(t *testing.T) {
-	//_ = os.RemoveAll("data")
-	sstore, err := Open(DefaultOptions("data").WithMaxMStreamTableSize(10 * MB))
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	var name = "stream1"
-	var data = strings.Repeat("hello world,", 10)
-	var wg sync.WaitGroup
-	for i := 0; i < 1000000; i++ {
-		wg.Add(1)
-		sstore.AsyncAppend(name, []byte(data), func(err error) {
-			if err != nil {
-				t.Fatalf("%+v", err)
-			}
-			wg.Done()
-		})
-	}
-	wg.Wait()
+	_ = os.RemoveAll("data")
+	defer func() {
+		_ = os.RemoveAll("data")
+	}()
+	for i := 0; i < 10; i++ {
+		sstore, err := Open(DefaultOptions("data").WithMaxMStreamTableSize(10 * MB))
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+		var name = "stream1"
+		var data = strings.Repeat("hello world,", 10)
+		var wg sync.WaitGroup
+		for i := 0; i < 100000; i++ {
+			wg.Add(1)
+			sstore.AsyncAppend(name, []byte(data), func(err error) {
+				if err != nil {
+					t.Fatalf("%+v", err)
+				}
+				wg.Done()
+			})
+		}
+		wg.Wait()
+		if err := sstore.Close(); err != nil {
+			t.Errorf("%+v", err)
+		}
 
-	if err := sstore.Close(); err != nil {
-		t.Errorf("%+v", err)
-	}
-
-	sstore, err = Open(DefaultOptions("data").WithMaxMStreamTableSize(MB))
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-	//fmt.Println(sstore.End("stream1"))
-	if err := sstore.Close(); err != nil {
-		t.Errorf("%+v", err)
+		sstore, err = Open(DefaultOptions("data").WithMaxMStreamTableSize(10 * MB))
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if err := sstore.Close(); err != nil {
+			t.Errorf("%+v", err)
+		}
 	}
 }
 
 func TestWalHeader(t *testing.T) {
 	os.RemoveAll("data")
+	defer func() {
+		os.RemoveAll("data")
+	}()
 	os.MkdirAll("data", 0777)
 	wal, err := openWal("data/1.log")
 	if err != nil {
